ca491c814fbc68839939fc3910bd5e9e5fe839ba
Nika Ptushkina <nika.ptushkina@machineryanalytics.com>
2022-02-01 19:05:49 -0800

    asserArraysEqual.js created

---
+++ assertArraysEqual.js
@@ -0,0 +1,13 @@
+const eqArrays = require('./eqArrays');
+const assertArraysEqual = function(actual, expected) {
+  if (eqArrays(actual, expected)) {
+    console.log(`✅✅✅ Assertion Passed: "${actual}" === "${expected}"`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: "${actual}" !== "${expected}"`);
+      }
+    };
+
+module.exports = assertArraysEqual;
+
+assertArraysEqual([1, 2, 3], [1, 2, 3])
+assertArraysEqual([1, 2, 3], [3, 2, 1]) 
