1bac19fa96f028c73f6f77643e6b5874d19b0d2d
Nika Ptushkina <nika.ptushkina@machineryanalytics.com>
2022-02-01 19:14:09 -0800

    Added without.js and flatten.js

---
+++ flatten.js
@@ -0,0 +1,15 @@
+const flatten = function(array) {
+  let flattenedArray = [];
+  for (let i = 0; i < array.length; i++) {
+    if (Array.isArray(array[i])) {
+      for (const element of array[i]) {
+        flattenedArray.push(element);
+      }
+    } else {
+      flattenedArray.push(array[i]);
+    }
+  }
+  return flattenedArray;
+};
+
+module.exports = flatten;
