f68b794076ed2e3be87e67484f9b0820ecdabafa
Nika Ptushkina <nika.ptushkina@machineryanalytics.com>
2022-02-05 23:37:35 -0800

    middle.js created

---
+++ middle.js
@@ -0,0 +1,45 @@
+const eqArrays = function(array1, array2) {
+  if (array1.length !== array2.length) {
+    return false;
+  }
+  for (let i = 0; i < array1.length; i++) {
+    if (Array.isArray(array1[i]) && (eqArrays(array1[i], array2[i]) === false)) {
+      return false;
+    }
+    if (!(Array.isArray(array1[i])) && array1[i] !== array2[i]) {
+      return false;
+    }
+  } 
+  return true;
+};
+
+const assertArraysEqual = function(actual, expected) {
+  if (eqArrays(actual, expected)) {
+    console.log(`✅✅✅ Assertion Passed: "${actual}" === "${expected}"`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: "${actual}" !== "${expected}"`);
+      }
+    };
+
+
+const middle = function(array) {
+  if (array.length < 3) {
+    return [];
+  } else if (array.length % 2 === 0) {
+    return array.slice(array.length / 2 - 1, array.length / 2 + 1)
+  } else {
+      let newArray = [];
+      let middleNum = Math.round(array.length /2)
+      newArray.push(array[middleNum -1]);
+      return newArray;
+   }
+};
+    
+module.exports = middle;
+
+console.log(middle([1])) // => []
+console.log(middle([1, 2])) // => []
+console.log(middle([1, 2, 3])) // => [2]
+console.log(middle([1, 2, 3, 4, 5])) // => [3]
+console.log(middle([1, 2, 3, 4])) // => [2, 3]
+console.log(middle([1, 2, 3, 4, 5, 6])) // => [3, 4]
