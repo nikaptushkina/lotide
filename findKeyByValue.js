1419b9fb1201d0c973eda811d7eaeae9354cad4e
Nika Ptushkina <nika.ptushkina@machineryanalytics.com>
2022-02-06 23:29:40 -0800

    added findKeyByValue.js

---
+++ findKeyByValue.js
@@ -0,0 +1,29 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const findKeyByValue = function(obj, objValue) {
+  let keys = Object.keys(obj);
+  let index = 0;
+  for (var filmType in obj) {
+    var str = obj[filmType];
+    if (str === objValue) {
+      return keys[index];
+    }
+    index++;
+  }
+  return undefined;
+};
+
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
