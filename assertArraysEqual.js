84705cf087251fb043fecd1539bf047ade85276c
Nika Ptushkina <nika.ptushkina@machineryanalytics.com>
2022-02-08 00:42:05 -0800

    added map.js

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -9,5 +9,7 @@ const assertArraysEqual = function(actual, expected) {
 
 module.exports = assertArraysEqual;
 
+/*
 assertArraysEqual([1, 2, 3], [1, 2, 3])
-assertArraysEqual([1, 2, 3], [3, 2, 1]) 
+assertArraysEqual([1, 2, 3], [3, 2, 1])
+*/
