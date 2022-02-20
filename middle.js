84581627ba3383d0967e55658ba6845b526c07f3
Nika Ptushkina <nika.ptushkina@gmail.com>
2022-02-19 16:01:47 -0800

    Refactored middle.js by exporting and creating test file.

--- middle.js
+++ middle.js
@@ -1,27 +1,8 @@
-const eqArrays = function(array1, array2) {
-  if (array1.length !== array2.length) {
-    return false;
-  }
-  for (let i = 0; i < array1.length; i++) {
-    if (Array.isArray(array1[i]) && (eqArrays(array1[i], array2[i]) === false)) {
-      return false;
-    }
-    if (!(Array.isArray(array1[i])) && array1[i] !== array2[i]) {
-      return false;
-    }
-  } 
-  return true;
-};
+// REQUIREMENTS
+const eqArrays = require('./eqArrays');
+const assertArraysEqual = require('./assertArraysEqual');
 
-const assertArraysEqual = function(actual, expected) {
-  if (eqArrays(actual, expected)) {
-    console.log(`✅✅✅ Assertion Passed: "${actual}" === "${expected}"`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: "${actual}" !== "${expected}"`);
-      }
-    };
-
-
+// FUNCTION
 const middle = function(array) {
   if (array.length < 3) {
     return [];
@@ -34,12 +15,6 @@ const middle = function(array) {
       return newArray;
    }
 };
-    
+ 
+// EXPORT
 module.exports = middle;
-
-console.log(middle([1])) // => []
-console.log(middle([1, 2])) // => []
-console.log(middle([1, 2, 3])) // => [2]
-console.log(middle([1, 2, 3, 4, 5])) // => [3]
-console.log(middle([1, 2, 3, 4])) // => [2, 3]
-console.log(middle([1, 2, 3, 4, 5, 6])) // => [3, 4]
